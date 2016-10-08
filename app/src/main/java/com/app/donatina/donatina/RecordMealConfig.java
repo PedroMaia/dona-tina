583d88b05a4967333f45551a1f9d3f568e85bb66
André Geraldes <andregeraldes@a88-157-228-119.cpe.netcabo.pt>
2016-10-08 02:15:11 +0100

    Added homescreen and meal config

---
+++ app/src/main/java/com/app/donatina/donatina/RecordMealConfig.java
@@ -0,0 +1,13 @@
+package com.app.donatina.donatina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordMealConfig extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_meal_config);
+    }
+}
